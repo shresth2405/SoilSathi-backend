55cade60e73f7766f8a881a67495997d20fd7fcf
shresth2405 <maheshwarishresth@gmail.com>
2025-09-03 19:10:42 +0530

    Added protected route

--- src/routes/Gardener.routes.js
+++ src/routes/Gardener.routes.js
@@ -7,6 +7,13 @@ const router = Router();
 router.route('/create').post(createGardener);
 router.route('/login').post(loginGardener);
 router.route('/addDevice').post(verifyJWT,addDeviceId);
+router.get("/protected", verifyJWT, (req, res) => {
+  res.json({
+    success: true,
+    message: "You accessed a protected route!",
+    user: req.user, // this comes from verifyJWT middleware
+  });
+});
 
 
 export default router;
